8618999ca52eebdde48058715a1dd395f1e6425c
imnotdeepak <rambutanandlycheejuice@gmail.com>
2025-10-17 00:06:27 -0400

    Highlights and footer section

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import Hero from "./components/Hero";
 import ProductViewer from "./components/ProductViewer";
 import Showcase from "./components/Showcase";
 import Performance from "./components/Performance";
+import Features from "./components/Features";
+import Highlights from "./components/Highlights";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
@@ -13,6 +16,9 @@ const App = () => {
       <ProductViewer />
       <Showcase />
       <Performance />
+      <Features />
+      <Highlights />
+      <Footer />
     </main>
   );
 };

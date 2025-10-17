b355fc73ff1757a0dd9e9ea18792d19a646736cc
imnotdeepak <rambutanandlycheejuice@gmail.com>
2025-10-16 23:43:35 -0400

    Showcase section with GSAP scroll animations

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import ProductViewer from "./components/ProductViewer";
+import Showcase from "./components/Showcase";
 
 const App = () => {
   return (
     <main>
       <Navbar />
       <Hero />
+      <ProductViewer />
+      <Showcase />
     </main>
   );
 };
